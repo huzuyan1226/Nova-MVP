d9e64fd03e7f72cf8afe355c72b0c9d587c281fc
Zuyan <huzuyan1226@gmail.com>
2025-09-16 09:44:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from datetime import datetime
 import streamlit as st
 
 from supabase import create_client
-
 supabase = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 
 # ---------- 基本配置 ----------
